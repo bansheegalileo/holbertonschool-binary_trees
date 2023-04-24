aa260d98cb0e6229b8604ccfb05998c7cbef63d7
root <root@MSI>
2023-04-24 13:54:44 -0500

    uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds uncle node
+ * @node: node ptr
+ * Return: ptr to unc
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
+
+    return (binary_tree_sibling(node->parent));
+}
+
+/**
+* binary_tree_sibling - finds node sib
+* @node: node ptr
+* Return: ptr to sib
+**/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
